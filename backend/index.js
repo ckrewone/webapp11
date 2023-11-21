de05e4b991a1866ecd8712719ad7d5b1256b403e
Michał Żakowski <ckrewone@o2.pl>
2023-11-21 10:52:41 +0000

    handle call

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,23 @@
 const express  = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const config = {
  url: 'https://uni-call.fcc-online.pl',
  login: 'focus08',
  password: '#h23rth564vl'
 };
+
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+ next();
+});
+
 
 dialer.configure(config); 
 
@@ -21,3 +32,30 @@ httpServer.get('/call/:number1/:number2', (req, res) => {
  dialer.call(number1,number2);
  res.json({success: true});
 })
+
+httpServer.post('/call/', async (req, res) => {
+    const number1 = req.body.number;
+    const number2 = '503777158' // tutaj dejemy swój numer
+    console.log('Dzwonie', number1, number2)
+    const bridge = await dialer.call(number1, number2);
+    let interval = setInterval(async () => {
+      let status = await bridge.getStatus();
+      console.log(status)
+      if (
+        status === "ANSWERED" ||
+        status === "FAILED" ||
+        status === "BUSY" ||
+        status === "NO ANSWER"
+      ) {
+        console.log("stop");
+        clearInterval(interval);
+      }
+    }, 2000);
+    res.json({ success: true });
+   })
+   
+   httpServer.get('/status', async function (req, res) {
+    let status = await bridge.getStatus();
+    res.json({ id: '123', "status": status });
+   })
+   
---
+++ front/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Start from '../views/StartView.vue'
+import Ringing from '../views/RingingView.vue'
+
+const routes = [
+ {
+   path: '/',
+   name: 'start',
+   component: Start
+ },
+ {
+  path: '/ringing',
+  name: 'ringing',
+  component: Ringing,
+  props: true
+}
+
+]
+
+const router = createRouter({
+ history: createWebHashHistory(),
+ routes
+})
+
+export default router
